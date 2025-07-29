bd2f4b03e957b917dbbf4521df12b17ebdea7183
Israr A. <91403838+Israr-11@users.noreply.github.com>
2025-07-29 20:02:31 +0100

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -7,6 +7,7 @@ class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
+    
     username = db.Column(db.String(80), unique=True, nullable=False)
     
     def __repr__(self):
